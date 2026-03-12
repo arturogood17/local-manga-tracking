4991f98d22f3b57f0b8ca4607fa80e3bff6c7cd2
arturogood17 <arturo_good17@hotmail.com>
2026-03-13 00:36:37 +0100

    Boceto de los handlers. Go.mod y go.sum se movieron al root

---
+++ internal/api/controllers/handlers.go
@@ -0,0 +1,93 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+// Handlers de usuarios
+
+func CreateUsers(c *gin.Context) {
+	// Handler para crear usuarios
+}
+
+func UpdateProfile(c *gin.Context) {
+	// Handler para actualizar el perfil del usuario
+}
+
+func GetProfile(c *gin.Context) {
+	// Handler para obtener el perfil del usuario
+}
+
+func ChangePassword(c *gin.Context) {
+	// Handler para actualizar la contraseña
+}
+
+func DeleteUser(c *gin.Context) {
+	// Handler para borrar usuario
+}
+
+// Handlers de resources
+func CreateResource(c *gin.Context) {
+	// Handler para crear una serie de manga/anime
+}
+
+func GetResource(c *gin.Context) {
+	// Handler para obtener una serie de manga/anime
+}
+
+func UpdateResource(c *gin.Context) {
+	// Handler para actualizar una serie de manga/anime
+}
+
+func DeleteResource(c *gin.Context) {
+	// Handler para borrar recurso
+}
+
+// Handlers para listas
+func CreateList(c *gin.Context) {
+	// Handler para crear una lista de manga/anime
+}
+
+func GetList(c *gin.Context) {
+	// Handler para obtener una lista de manga/anime
+}
+
+func UpdateList(c *gin.Context) {
+	// Handler para actualizar una lista de manga/anime
+}
+
+func DeleteList(c *gin.Context) {
+	// Handler para borrar una lista
+}
+
+// Handlers para puntuaciones
+func CreateScore(c *gin.Context) {
+	// Handler para crear una puntuación de un manga/anime
+}
+
+func GetScore(c *gin.Context) {
+	// Handler para obtener una puntuación de un manga/anime
+}
+
+func UpdateScore(c *gin.Context) {
+	// Handler para actualizar una puntuación de un manga/anime
+}
+
+func DeleteScore(c *gin.Context) {
+	// Handler para borrar una puntuación de un manga/anime
+}
+
+// Handlers para reseñas
+func CreateReview(c *gin.Context) {
+	// Handler para crear una reseña de un manga/anime
+}
+
+func GetReview(c *gin.Context) {
+	// Handler para obtener una reseña de un manga/anime
+}
+
+func UpdateReview(c *gin.Context) {
+	// Handler para actualizar una reseña de un manga/anime
+}
+
+func DeleteReview(c *gin.Context) {
+	// Handler para borrar una reseña de un manga/anime
+}
